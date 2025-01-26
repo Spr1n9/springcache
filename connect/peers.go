4091d4f287725fce64f65e9cc248e17533f2c17f
springLiao <132885112+Spr1n9@users.noreply.github.com>
2025-01-26 11:48:51 +0800

    Add files via upload

--- connect/peers.go
+++ connect/peers.go
@@ -10,6 +10,7 @@ type PeerPicker interface {
 }
 
 // PeerGetter 定义了从远端获取缓存的能力,Client
+// 在connect.client 包中， 定义了结构体Client, 它有下面的Get方法和Set方法，满足了下面的接口，所以可以作为PeerGetter被使用
 type PeerGetter interface {
 	Get(group string, key string) ([]byte, error)
 	Set(group string, key string, value []byte, expire time.Time, ishot bool) error
